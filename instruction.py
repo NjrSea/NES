8dfb77f743f6baf0fd71197570af6461c6d0eadf
wang ya <576330572@qq.com>
2018-05-04 09:39:46 +0800

    implement instructions

--- instruction.py
+++ instruction.py
@@ -1,5 +1,13 @@
 from abc import ABC, abstractmethod, abstractproperty
 
+"""
+Finished Instructions
+
+LDA #
+
+SEI
+CLD
+"""
 
 class Instruction(ABC):
     def __init__(self):
@@ -18,29 +26,42 @@ class Instruction(ABC):
         return 1
 
     @abstractmethod
-    def execute(self):
+    def execute(self, *args):
         print(self.__str__())
 
 
-class LDAInstruction(Instruction):
+# data instructions
+class LDAImmInstruction(Instruction):
     identifier_byte = bytes.fromhex('A9')
     instruction_length = 2
 
-    def execute(self):
-        super().execute()
+    def execute(self, cpu, data_bytes):
+        # load value into accumulator register
+        cpu.a_reg = data_bytes
 
 
+# status instructions
 class SEIInstruction(Instruction):
     identifier_byte = bytes.fromhex('78')
     instruction_length = 1
 
-    def execute(self):
-        super().execute()
+    def execute(self, cpu, data_bytes):
+        # set the instruction flag to 1
+        cpu.status_reg.interrupt = True
 
 
 class CLDInstruction(Instruction):
     identifier_byte = bytes.fromhex('D8')
     instruction_length = 1
 
-    def execute(self):
-        super().execute()
+    def execute(self, cpu, data_bytes):
+        cpu.status_reg.decimal_bit = False
+
+
+class STAAbsInstruction(Instruction):
+    identifier_byte = bytes.fromhex('8D')
+    instruction_length = 3
+
+    def execute(self, cpu, data_bytes):
+        # take value from A register and put it in memory
+        memory_address = data_bytes[1:2] + data_bytes[0:1]  # plus to convert endianess
