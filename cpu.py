a76c237d68b4c085454c476ac968f8296a098c57
wang ya <576330572@qq.com>
2018-05-04 09:37:25 +0800

    refactor cpu

--- cpu.py
+++ cpu.py
@@ -1,4 +1,4 @@
-from instruction import Instruction, LDAInstruction, SEIInstruction, CLDInstruction
+from instruction import Instruction, LDAImmInstruction, SEIInstruction, CLDInstruction, STAAbsInstruction
 from rom import ROM
 from status import Status
 
@@ -6,7 +6,7 @@ from status import Status
 class CPU:
     def __init__(self):
         # status registers: store a single byte
-        self.status_reg = None   # status register
+        self.status_reg = None   # type: Status
 
         # counter registers: store a single byte
         self.pc_reg = None  # program counter
@@ -20,16 +20,17 @@ class CPU:
         # program counter stores current execution point
         self.running = False
 
-        self.instruction_classes = [
-            SEIInstruction,
-            CLDInstruction,
-            LDAInstruction,
+        self.instructions = [
+            SEIInstruction(),
+            CLDInstruction(),
+            LDAImmInstruction(),
+            STAAbsInstruction(),
         ]
 
-        self.instruction_class_mapping = dict()
+        self.instruction_mapping = dict()
 
-        for instruction_class in self.instruction_classes:
-            self.instruction_class_mapping[instruction_class.identifier_byte] = instruction_class
+        for instruction_class in self.instructions:
+            self.instruction_mapping[instruction_class.identifier_byte] = instruction_class
 
         self.rom = None
 
@@ -42,10 +43,9 @@ class CPU:
         stack pointer: $FD
         $4017: 0 (sound chanel disabled)
         $4015: 0 (frame IRQ disabled)
-        $4000-$400F: 0 (sound registers)
-        """
+        $4000-$400F: 0 (sound registers) """
         self.pc_reg = 0
-        self.status_reg = Status(bytes.fromhex('34'))
+        self.status_reg = Status()
         self.sp_reg = bytes.fromhex('FD')
 
         # TODO change to int?
@@ -64,18 +64,22 @@ class CPU:
         self.running = True
         while self.running:
             # get the current byte at pc
-            identifier_byte = self.rom.get_byte(self.pc_reg)
+            identifier_byte = self.rom.get_bytes(self.pc_reg)
 
             # turn the byte into an Instruction
             # mapping[identifier_byte] will crash if identifier_byte is not valid
             # get method with default value of None can get value safely
-            instruction_class = self.instruction_class_mapping.get(identifier_byte, None)
-            if instruction_class is None:
+            instruction: Instruction = self.instruction_mapping.get(identifier_byte, None)
+            if instruction is None:
                 raise Exception("Instruction not found")
 
+            # get the correct amount of data bytes
+            num_data_bytes = instruction.instruction_length - 1
+            # get the data bytes
+            data_bytes = self.rom.get_bytes(self.pc_reg + 1, num_data_bytes)
+
             # we have a valid instruction
-            instruction = instruction_class()
-            instruction.execute()
+            instruction.execute(self, data_bytes)
 
             self.pc_reg += instruction.instruction_length
 
