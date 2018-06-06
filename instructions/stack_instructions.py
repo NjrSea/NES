54e41ad5c3e3d0077dcc39da01ed9aff85fceafd
wang ya <576330572@qq.com>
2018-06-06 09:36:41 +0800

    Fix Tsx and Txs

--- instructions/stack_instructions.py
+++ instructions/stack_instructions.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from addressing import ImplicitAddressing
 from instructions.generic_instruction import Instruction
 from instructions.base_instructions import StackPush, StackPull
@@ -24,15 +26,17 @@ class Pha(ImplicitAddressing, StackPush):
         return cpu.a_reg
 
 
-class Txs(ImplicitAddressing, StackPush):
-    sets_negative_bit = True
+class Txs(ImplicitAddressing, Instruction):
+    # TODO:
+    # sets_negative_bit = True
     sets_zero_bit = True
 
     identifier_byte = bytes([0x9A])
 
     @classmethod
-    def data_to_push(cls, cpu):
-        return cpu.x_reg
+    def write(cls, cpu: 'cpu.CPU', memory_address, value):
+        cpu.sp_reg = cpu.x_reg
+        return cpu.sp_reg
 
 
 # stack pull instructions
@@ -56,17 +60,17 @@ class Pla(ImplicitAddressing, StackPull):
 
     @classmethod
     def write_pulled_data(cls, cpu, pulled_data):
-        cpu.a_reg = pulled_data
+        cpu.a_reg = np.uint8(pulled_data)
         return cpu.a_reg
 
 
-class Tsx(ImplicitAddressing, StackPull):
+class Tsx(ImplicitAddressing, Instruction):
     sets_negative_bit = True
     sets_zero_bit = True
 
     identifier_byte = bytes([0xBA])
 
     @classmethod
-    def write_pulled_data(cls, cpu, pulled_data):
-        cpu.x_reg = pulled_data
+    def write(cls, cpu: 'cpu.CPU', memory_address, value):
+        cpu.x_reg = cpu.sp_reg
         return cpu.x_reg
